527b8e7c4a7788b6e879e780cc0c14433b7e5121
Mohamed-Alasfoor <158215288+Mohamed-Alasfoor@users.noreply.github.com>
2024-08-20 23:13:06 +0300

    Added queries

---
+++ Database/user.go
@@ -0,0 +1,26 @@
+package Database
+
+import (
+	"database/sql"
+	"talknet/structs"
+	"time"
+)
+
+// CreateUser inserts a new user into the database.
+func CreateUser(db *sql.DB, username, email, password string) error {
+	_, err := db.Exec("INSERT INTO users (username, email, password, created_at) VALUES (?, ?, ?, ?)",
+		username, email, password, time.Now())
+	return err
+}
+
+// GetUserByUsername retrieves a user by username.
+
+func GetUserByUsername(db *sql.DB, username string) (structs.User, error) {
+	row := db.QueryRow("SELECT id, username, email, password, created_at FROM users WHERE username = ?", username)
+
+	var user structs.User
+	err := row.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt)
+	return user, err
+}
+
+// Other user-related functions (e.g., UpdateUser, DeleteUser) go here.
